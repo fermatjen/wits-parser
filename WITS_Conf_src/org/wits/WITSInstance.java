486b1d2f78bdab661513328515f683ed5b36c5d1
frank.jennings <frank.jennings@3bf8f6c8-daf2-11dd-a297-4fb579711a53>
2009-03-13 08:44:20 +0000

    Added the missing WITSInstance class

---
+++ WITS_Conf_src/org/wits/WITSInstance.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+ *
+ *
+ * WITS - Wiki to Structured Markup Converter.
+ *
+ * Copyright (C) 2009 by Frank Jennings (fermatjen@yahoo.com).
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation under the terms of the GNU General Public License is hereby
+ * granted. No representations are made about the suitability of this software
+ * for any purpose. It is provided "as is" without express or implied warranty.
+ * See the GNU General Public License for more details.
+ *
+ * Documents produced by WITS converter are derivative works derived from the
+ * input used in their production; they are not affected by this license.
+ *
+ */
+
+package org.wits;
+
+/**
+ *
+ * @author phantom
+ */
+public class WITSInstance {
+
+    private String inputType = "confluence";
+    private String outputType = "solbook";
+
+    public String getInputType() {
+        return inputType;
+    }
+
+    public void setInputType(String inputType) {
+        this.inputType = inputType;
+    }
+
+    public String getOutputType() {
+        return outputType;
+    }
+
+    public void setOutputType(String outputType) {
+        this.outputType = outputType;
+    }
+
+
+
+}
